bbb48683cf59880e7f9a12dd10cc0c84d7d2524b
Lovro Iljas <lovro.iljas@gmail.com>
2022-01-15 15:49:50 +0100

    Added MongoDb repository and configured the settings.

--- CatalogApp/Repositories/MongoDbItemsRepository.cs
+++ CatalogApp/Repositories/MongoDbItemsRepository.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using CatalogApp.Models;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CatalogApp.Repositories
 {
-	public class MongoDbItemsRepository
+	public class MongoDbItemsRepository : IItemsRepository
 	{
+		private const string databaseName = "catalog";
+		private const string collectionName = "items";
+		private readonly IMongoCollection<Item> itemsCollection;
 
+		public MongoDbItemsRepository(IMongoClient mongoClient)
+		{
+			IMongoDatabase database = mongoClient.GetDatabase(databaseName);
+			itemsCollection = database.GetCollection<Item>(collectionName);
+		}
+
+		public void CreateItem(Item item)
+		{
+			itemsCollection.InsertOne(item);
+		}
+
+		public void DeleteItem(Guid id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Item GetItem(Guid id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Item> GetItems()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void UpdateItem(Item item)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
---
+++ CatalogApp/Settings/MongoDbSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogApp.Settings
+{
+	public class MongoDbSettings
+	{
+		public string Host { get; set; }
+		public int Port { get; set; }
+
+		public string CollectionString 
+		{
+			get 
+			{
+				return $"mongodb://{Host}:{Port}";
+			} 
+		}
+	}
+}
