d41d6a8106fc7a6ee1fbbd56c8b7e1604f0511db
Lovro Iljas <lovro.iljas@gmail.com>
2022-03-10 15:37:53 +0100

    Switched to MongoDB repository and serialized the Guid and DateTime types.

--- CatalogApp/Startup.cs
+++ CatalogApp/Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
@@ -29,12 +32,16 @@ namespace CatalogApp
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+			BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
 			services.AddSingleton<IMongoClient>(serviceProvider => 
 			{
 				var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
 				return new MongoClient(settings.CollectionString);
 			});
-			services.AddSingleton<IItemsRepository, FileBasedRepository>();
+
+			services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 			services.AddControllers();
 			services.AddSwaggerGen(c =>
 			{
